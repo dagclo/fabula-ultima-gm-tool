4cf45a344a79e5bf4879dd64c48864493c3a04b5
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2024-06-24 19:09:56 -0700

    Implemented HP/MP calculations

--- FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/AttributeEdit.cs
+++ FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/AttributeEdit.cs
@@ -1,6 +1,8 @@
 using FabulaUltimaNpc;
+using FirstProject.Beastiary;
 using Godot;
 using System;
+using System.Collections.Generic;
 
 public partial class AttributeEdit : Control, IBeastAttribute
 {
@@ -9,11 +11,38 @@ public partial class AttributeEdit : Control, IBeastAttribute
     [Export]
     public string AttributeName { get; set; }
 
+    [Signal]
+    public delegate void AttributeNameSetEventHandler(string attributeName);
+
     public Action<System.Collections.Generic.ISet<BeastEntryNode.Action>> BeastTemplateAction { get; set; }
 
     public void HandleBeastChanged(IBeastTemplate beastTemplate)
     {
         if (beastTemplate == null) return;
         _beastTemplate = beastTemplate;
+        EmitSignal(SignalName.AttributeNameSet, AttributeName.ShortenAttribute());
+    }
+
+    public void HandleDiceSizeChanged(int newSize)
+    {
+        if(_beastTemplate == null) return;
+        switch(AttributeName.ToLowerInvariant())
+        {
+            case "might":
+                _beastTemplate.Might = new Die(newSize); 
+                break;
+            case "insight":
+                _beastTemplate.Insight = new Die(newSize);
+                break;
+            case "willpower":
+                _beastTemplate.WillPower = new Die(newSize);
+                break;
+            case "dexterity":
+                _beastTemplate.Dexterity = new Die(newSize);
+                break;
+            default:
+                return;
+        }
+        BeastTemplateAction.Invoke(new HashSet<BeastEntryNode.Action>() { BeastEntryNode.Action.CHANGED });
     }
 }
--- FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/DieOption.cs
+++ FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/DieOption.cs
@@ -5,18 +5,22 @@ public partial class DieOption : OptionButton
     [Export]
     public Godot.Collections.Array<int> DiceSizes { get; set; } = new Godot.Collections.Array<int>() { 6, 8, 10, 12 };
 
+    [Signal]
+    public delegate void DieSizeChangedEventHandler(int size);
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
-    {
-        RemoveItem(0);
+    {   
         foreach (var val in DiceSizes)
         {
             AddItem($"d{val}", val);
         }
+        Select(0);
     }
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
-	{
-	}
+    public void OnSelected(int index)
+    {
+        var size = GetItemId(index);
+        EmitSignal(SignalName.DieSizeChanged, size);
+    }
 }
