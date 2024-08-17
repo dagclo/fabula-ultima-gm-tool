4ecbff0a2cbb31487e1e216390e36565883c4950
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2024-08-16 17:30:41 -0700

    Scroll back up to top when search/filter done

---
+++ FabulaUltimaCampaignManager/Beastiary/BeastListScroll.cs
@@ -0,0 +1,21 @@
+using FirstProject.Messaging;
+using Godot;
+using System;
+using System.Threading.Tasks;
+
+public partial class BeastListScroll : ScrollContainer
+{
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+        var messageRouter = GetNode<MessageRouter>("/root/MessageRouter");
+        messageRouter.RegisterSubscriber<BeastiaryRefreshMessage>(this.ReceiveRefreshMessage);
+    }
+
+    private Task ReceiveRefreshMessage(IMessage message)
+    {
+        if (!(message is IMessage<BeastiaryRefreshMessage> refreshMessage)) return Task.CompletedTask;        
+        CallDeferred(MethodName.SetVScroll, (int)GetVScrollBar().MinValue);
+        return Task.CompletedTask;
+    }   
+}
