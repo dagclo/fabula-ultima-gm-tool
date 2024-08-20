b718342ad286f974ada61209e7b785aecf1ecd80
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2024-08-20 16:34:36 -0700

    Show Turns left in Tab Title

--- FabulaUltimaCampaignManager/Battle/FinishTurnButton.cs
+++ FabulaUltimaCampaignManager/Battle/FinishTurnButton.cs
@@ -2,11 +2,23 @@ using FirstProject.Beastiary;
 using FirstProject.Encounters;
 using FirstProject.Npc;
 using Godot;
+using System;
+using System.Linq;
 
 public partial class FinishTurnButton : Button, INpcReader, INpcStatusReader
 {
     private NpcInstance _instance;
     private BattleStatus _status;
+    private string _configuredText;
+
+    [Export]
+    public string TurnCounterCharacter { get; set; } = "*";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        _configuredText = this.Text;        
+    }
 
     public void HandleNpcChanged(NpcInstance npc)
     {
@@ -24,11 +36,23 @@ public partial class FinishTurnButton : Button, INpcReader, INpcStatusReader
 	public void OnButtonPressed() 
 	{
         _status.DecrementTurns();
-        this.Disabled = _status.IsActive;		
+        this.Disabled = !_status.IsActive;
 	}
 
     public void HandleStatusSet(BattleStatus status)
-    {        
-        _status = status;        
+    {
+        _status = status;
+        SetTitle();
+        _status.StatusChanged += OnStatusChanged;
+    }
+
+    private void OnStatusChanged(BattleStatus status)
+    {
+        SetTitle();
+    }
+
+    private void SetTitle()
+    {
+        Text = $"{_configuredText} {string.Join("", Enumerable.Range(1, _status.NumTurnsLeft).Select(_ => TurnCounterCharacter))}";
     }
 }
