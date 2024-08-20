e1e8ce36e1bdae5a9451cadd95774cc2cc196279
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2024-08-20 16:11:45 -0700

    Keep Track of Turns left In Status

--- FabulaUltimaCampaignManager/Battle/FinishTurnButton.cs
+++ FabulaUltimaCampaignManager/Battle/FinishTurnButton.cs
@@ -8,17 +8,6 @@ public partial class FinishTurnButton : Button, INpcReader, INpcStatusReader
     private NpcInstance _instance;
     private BattleStatus _status;
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-	{
-		
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
-	}
-
     public void HandleNpcChanged(NpcInstance npc)
     {
 		this._instance = npc;
@@ -29,13 +18,13 @@ public partial class FinishTurnButton : Button, INpcReader, INpcStatusReader
 		if (this._instance == null) return;
         if (this._status == null) return;
         this.Disabled = false;
-        _status.IsActive = true;
+        _status.ResetTurns();
     }
 
 	public void OnButtonPressed() 
 	{
-		this.Disabled = true;
-		_status.IsActive = false;
+        _status.DecrementTurns();
+        this.Disabled = _status.IsActive;		
 	}
 
     public void HandleStatusSet(BattleStatus status)
