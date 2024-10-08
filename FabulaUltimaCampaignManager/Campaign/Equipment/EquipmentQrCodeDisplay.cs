589cde777c0db27198675b581c3db803479f026a
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2024-10-07 20:14:36 -0700

    Try to Add Unit Test

--- FabulaUltimaCampaignManager/Campaign/Equipment/EquipmentQrCodeDisplay.cs
+++ FabulaUltimaCampaignManager/Campaign/Equipment/EquipmentQrCodeDisplay.cs
@@ -1,3 +1,4 @@
+using FabulaUltimaGMTool.Adaptors;
 using FirstProject.Beastiary;
 using FirstProject.Npc;
 using Godot;
@@ -14,77 +15,7 @@ public partial class EquipmentQrCodeDisplay : TextureRect, INpcEquipmentReader
 	{
 	}
 
-	private static string ToEnglish(int num)
-	{
-		switch (num)
-		{
-			case 1: return "One";
-            case 2: return "Two";
-			default:
-				throw new NotImplementedException();
-        }
-	}
 
-    public static dynamic ToDataFormat(NpcEquipment npcEquipment)
-    {
-        string type;
-        bool checkHandedNess = false;
-        string rangeValue = null;
-        int? defaultModValue = null;
-        string emptyStringDefault = null;
-        int? emptyIntDefault = null;
-        if (npcEquipment.Category.IsWeapon)
-        {
-            type = "Weapon";
-            checkHandedNess = true;
-            rangeValue = npcEquipment.BasicAttack.IsRanged ? "Ranged" : "Melee";
-            defaultModValue = 0;
-        }
-        else if (npcEquipment.Category.Name == "Shield")
-        {
-            type = "Shield";
-            emptyStringDefault = string.Empty;
-            rangeValue = emptyStringDefault;
-            emptyIntDefault = 0;
-        }
-        else if (npcEquipment.Category.IsArmor)
-        {
-            type = null;
-        }
-        else
-        {
-            type = "Accessory";
-        }
-
-
-        var equipment = new
-        {
-            name = npcEquipment.Name,
-            cost = npcEquipment.Cost,
-            quality = npcEquipment.Quality,
-            type = type,
-            accuracy = npcEquipment.BasicAttack != null ? $"\u3010{npcEquipment.BasicAttack.Attribute1?.ShortenAttribute()} + {npcEquipment.BasicAttack.Attribute2?.ShortenAttribute()}\u3011" : emptyStringDefault,
-            damage = npcEquipment.BasicAttack != null ? $"\u3010HR + {npcEquipment.BasicAttack.DamageMod}\u3011{npcEquipment.BasicAttack.DamageType.Name}" : emptyStringDefault,
-            handedness = checkHandedNess ? $"{ToEnglish(npcEquipment.NumHands)} Handed" : emptyStringDefault,
-            range = rangeValue,
-            martial = npcEquipment.IsMartial,
-            category = npcEquipment.Category.IsWeapon ? npcEquipment.Category.Name : null,
-            defense = npcEquipment.Category.IsWeapon ? (npcEquipment.Modifiers?.DefenseModifier ?? defaultModValue) : null,
-            mDefense = npcEquipment.Category.IsWeapon ? (npcEquipment.Modifiers?.MagicDefenseModifier ?? defaultModValue) : null,
-            dice1 = npcEquipment.BasicAttack?.Attribute1?.ShortenAttribute() ?? emptyStringDefault,
-            dice2 = npcEquipment.BasicAttack?.Attribute2?.ShortenAttribute() ?? emptyStringDefault,
-            accuracyConstant = npcEquipment.BasicAttack?.AttackMod ?? emptyIntDefault,
-            damageConstant = npcEquipment.BasicAttack?.DamageMod ?? emptyIntDefault,
-            basic = true,
-            defenseDice = npcEquipment.Category.IsArmor && !npcEquipment.Modifiers.DefenseOverrides ? "DEX" : null,
-            defenseConstant = npcEquipment.Category.IsArmor ? npcEquipment.Modifiers.DefenseModifier : (int?)null,
-            mDefenseDice = npcEquipment.Category.IsArmor ? "INS" : null,
-            mDefenseConstant = npcEquipment.Category.IsArmor ? npcEquipment.Modifiers.DefenseModifier : (int?)null,
-            initiative = npcEquipment.Category.IsArmor ? npcEquipment.Modifiers.InitiativeModifier : (int?)null,
-        };
-
-        return equipment;
-    }
 
     private static string ToJson(dynamic data)
 	{
@@ -115,7 +46,7 @@ public partial class EquipmentQrCodeDisplay : TextureRect, INpcEquipmentReader
 	public void HandleEquipmentChanged(NpcEquipment equipment)
 	{
 		if (!IsValid(equipment)) return;
-		var data = ToDataFormat(equipment);
+		var data = PHSAdapter.ToDataFormat(equipment);
 		var json = ToJson(data);
 		GD.Print(json);
         var plainTextBytes = System.Text.Encoding.UTF8.GetBytes(json);
