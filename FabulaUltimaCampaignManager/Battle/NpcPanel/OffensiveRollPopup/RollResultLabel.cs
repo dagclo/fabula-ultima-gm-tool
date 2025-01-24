f23b00fa7a885e4e55f77cd5a4eaf24a31882b2b
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2025-01-23 17:30:12 -0800

    Fix build

--- FabulaUltimaCampaignManager/Battle/NpcPanel/OffensiveRollPopup/RollResultLabel.cs
+++ FabulaUltimaCampaignManager/Battle/NpcPanel/OffensiveRollPopup/RollResultLabel.cs
@@ -29,7 +29,7 @@ public partial class RollResultLabel : RichTextLabel, INpcReader
     public void OnResultReady(SignalWrapper<CheckResult> signalWrapper)
     {
         var checkResult = signalWrapper.Value;
-        var log = checkResult.ToEncounterLog(_checkModel.Action, _checkModel.DamageType, _instance.InstanceName);
+        var log = checkResult.ToEncounterLog(_checkModel.Action, _instance.InstanceName, _checkModel.DamageType);
         _messagePublisher.Publish(log.AsMessage());
         this.Text = string.Empty;
         this.AppendText(checkResult.Success ? $"[color=green]Success[/color] for {checkResult.FinalHighRoll} damage" : "[color=red]Failed[/color]");
