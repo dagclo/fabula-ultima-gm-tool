63a939c8efcdb48cbe3a20e98ada89087c6453e5
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2025-01-25 14:49:37 -0800

    Implement updating damage modifier and type

---
+++ FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/Spells/SpellDamageModifierEdit.cs
@@ -0,0 +1,50 @@
+using FabulaUltimaNpc;
+using FirstProject.Beastiary;
+using Godot;
+using System;
+
+public partial class SpellDamageModifierEdit : LineEdit
+{
+    private SpellTemplate _spell;
+
+    public void HandleSpellSet(SignalWrapper<SpellTemplate> signal, bool editable)
+    {
+        this.Text = $"{signal.Value.DamageModifier ?? 0}";
+        _lastInput = this.Text;
+        this.Editable = editable;
+        _spell = signal.Value;
+    }
+
+    private string _lastInput = null;
+    public void HandleTextChanged(string newText)
+    {
+        bool changeSuccess = false;
+        if (!string.IsNullOrWhiteSpace(newText) && int.TryParse(newText, out var mpCost))
+        {
+            _spell.DamageModifier = mpCost;
+            changeSuccess = true;
+        }
+        if (changeSuccess)
+        {
+            _lastInput = newText;
+        }
+        else
+        {
+            this.Text = _lastInput;
+        }
+    }
+
+    public void HandleToggled(bool on)
+    {
+        if (on)
+        {
+            this.Visible = true;
+            _spell.DamageModifier = int.Parse(_lastInput);
+        }
+        else
+        {
+            this.Visible = false;
+            _spell.DamageModifier = null;
+        }        
+    }
+}
---
+++ FabulaUltimaCampaignManager/BeastiaryScenes/NPCSheet/Spells/SpellDamageTypeOptionsButton.cs
@@ -0,0 +1,62 @@
+using FabulaUltimaNpc;
+using FabulaUltimaSkillLibrary;
+using FirstProject.Beastiary;
+using Godot;
+using System;
+using System.Linq;
+
+public partial class SpellDamageTypeOptionsButton : OptionButton
+{
+    private SpellTemplate _spellTemplate;
+    private FabulaUltimaNpc.DamageType _current = null;
+
+    [Export]
+    public Godot.Collections.Array<string> DamageTypes { get; set; } = new Godot.Collections.Array<string>(DamageConstants.DamageTypes);
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        foreach ((var val, int index) in DamageTypes.Select((a, i) => (a, i)))
+        {
+            AddItem(val, index);
+        }
+        Select(-1);
+    }
+
+    public void OnSelected(int index)
+    {
+        if (_spellTemplate == null) return;
+        var damageTypeName = DamageTypes[GetItemId(index)];
+        _spellTemplate.DamageType = new FabulaUltimaNpc.DamageType
+        {
+            Id = DamageConstants.DamageTypeMap[damageTypeName],
+            Name = damageTypeName,
+        };
+        _current = _spellTemplate.DamageType;
+    }
+
+    public void HandleSpellSet(SignalWrapper<SpellTemplate> signal, bool editable)
+    {
+        var spell = signal.Value;
+        _spellTemplate = spell;
+        this.Disabled = !editable;
+        if (_spellTemplate.DamageType == null) return;
+        var id = DamageTypes.IndexOf(_spellTemplate.DamageType.Name.ToLowerInvariant());
+        Select(GetItemIndex(id));
+        _current = _spellTemplate.DamageType;
+    }
+
+    public void HandleToggled(bool on)
+    {
+        if (on)
+        {
+            this.Visible = true;
+            _spellTemplate.DamageType = _current;
+        }
+        else
+        {
+            this.Visible = false;
+            _spellTemplate.DamageType = null;
+        }
+    }
+}
