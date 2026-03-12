4e6e428949946d19305b876d5d73a24744efc5ab
Chidozie Nwobilor <chidozie.nwobilor@gmail.com>
2026-03-12 08:36:09 -0700

    Fix show/hide clocks; make clocks cascade

--- FabulaUltimaCampaignManager/Battle/ProgressClocks/ProgressClockContainer.cs
+++ FabulaUltimaCampaignManager/Battle/ProgressClocks/ProgressClockContainer.cs
@@ -18,10 +18,16 @@ public partial class ProgressClockContainer : VBoxContainer
     [Export]
     public string ShowModeGroupName { get; set; } = "show_progress_clock";
 
+    [Export]
+    public int XCascadeOffset { get; set; } = 50;
+
+    [Export]
+    public int YCascadeOffset { get; set; } = 50;
+
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
 	{
-        _progressClockModels = GetNode<RunState>("/root/RunState").RunningEncounter.ProgressClocks;
+        _progressClockModels = new Array<ProgressClockModel>();
         _progressClockModels.Add(new ProgressClockModel
         {
             Title = "Players Escape (generated)",
@@ -33,14 +39,25 @@ public partial class ProgressClockContainer : VBoxContainer
             SectionStates = [false, false, false, false]
         });
 
+        _progressClockModels.AddRange(GetNode<RunState>("/root/RunState").RunningEncounter.ProgressClocks);
+
+        var curYOffSet = 0;
+        var curXOffSet = 0;
+
         foreach (var clockModel in _progressClockModels)
         {
             var progressClock = ProgressClockTemplate.Instantiate<ProgressClock>();
+            progressClock.Visible = false;
             progressClock.Model = clockModel;
             progressClock.OnHideClock += (ProgressClock c) => HandleHideClock(c);
-            AddChild(progressClock);
             progressClock.Owner = this;
-            progressClock.AddToGroup(ShowModeGroupName);            
+            progressClock.DeleteDisabled = true;
+            progressClock.AddToGroup(ShowModeGroupName);
+            progressClock.InitialPosition = Window.WindowInitialPosition.Absolute;
+            progressClock.Position = progressClock.Position + new Vector2I(curXOffSet, curYOffSet);            
+            AddChild(progressClock);
+            curYOffSet += YCascadeOffset;
+            curXOffSet += XCascadeOffset;
         }
 
         ShowHide(false);
@@ -48,7 +65,7 @@ public partial class ProgressClockContainer : VBoxContainer
 
     private void HandleHideClock(ProgressClock c)
     {
-        c.Visible = false;
+        // do nothing for now
     }
 
     private void HandleHidePressed()
@@ -58,14 +75,26 @@ public partial class ProgressClockContainer : VBoxContainer
 
     private void ShowHide(bool showMode)
     {
-        foreach (var control in GetTree().GetNodesInGroup(ShowModeGroupName).Select(n => n as Control))
+        void SetGroupVisibility(Node node, bool visible)
         {
-            control.Visible = showMode;
+            if (node is Control control)
+            {
+                control.Visible = visible;
+            }
+            else if (node is Window window)
+            {
+                window.Visible = visible;
+            }
         }
 
-        foreach (var control in GetTree().GetNodesInGroup(HideModeGroupName).Select(n => n as Control))
+        foreach (var node in GetTree().GetNodesInGroup(ShowModeGroupName))
         {
-            control.Visible = !showMode;
+            SetGroupVisibility(node, showMode);
+        }
+
+        foreach (var node in GetTree().GetNodesInGroup(HideModeGroupName))
+        {            
+            SetGroupVisibility(node, !showMode);
         }
     }
 
